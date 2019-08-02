faec4eb1e4f52ce2955e7604fc5cbcb7a59220c6
Trevor Narayan <trevor.narayan@gmail.com>
2019-08-01 23:35:53 -0700

    refactoring, pulling more stuff out of js

--- assets/js/rsvp.js
+++ assets/js/rsvp.js
@@ -1,3 +1,52 @@
+const SEARCH_URL = "https://us-central1-round-carver-683.cloudfunctions.net/searchInvites";
+const RSVP_URL = "https://us-central1-round-carver-683.cloudfunctions.net/saveRSVP";
+
+function searchInvites() {
+  var firstname = $('#firstname').val();
+  var lastname = $('#lastname').val();
+  var searchUrl = "SEARCH_URL" + "?firstname=" + firstname +
+                  "&lastname=" + lastname;
+  $.get(searchUrl, function(data, status) {
+    var row = data;
+    if (row != '') {
+      displyWelcome(row[0]);
+      appendForm(row);
+    } else {
+      $('#error').css("display", "block");
+    }
+  });
+}
+
+function displayWelcome(name) {
+  $('#welcome').prepend(name + '<br>');
+  $('#welcome').css("display", "block");
+}
+
+function appendForm(row) {
+  if (row[3] > 0) {
+    var friday_html = generateSelectorHtml('Rehearsal Dinner', row[3], 'friday');
+    var saturday_html = generateSelectorHtml('Wedding & Reception', row[4], 'saturday');
+    var sunday_html = generateSelectorHtml('Sunday Brunch', row[5], 'sunday');
+    $('#forms').html(friday_html);
+    $('#forms').append(saturday_html);
+    $('#forms').append(sunday_html);
+  } else if (row[3] == 0 && row[5] > 0) {
+    var saturday_html = generateSelectorHtml('Wedding & Reception', row[4], 'saturday');
+    var sunday_html = generateSelectorHtml('Sunday Brunch', row[5], 'sunday');
+    $('#forms').html(saturday_html);
+    $('#forms').append(sunday_html);
+  } else {
+    var saturday_html = generateSelectorHtml('Wedding & Reception', row[4], 'saturday');
+    $('#forms').html(saturday_html);
+  }
+  $('#forms').append("<br><br>Please inform us of any dietary restrictions:  " +
+                     "<input id='diet'></input>");
+  $('#forms').append("<br><br><button type='button' id='submitrsvp'>Submit RSVP!</button>");
+  $('#submitrsvp').click(function() {
+    submitRSVP(row[0]);
+  });
+}
+
 function generateSelectorHtml(label, numGuests, id) {
   var base = "<br>" + label + ": <select id='" + id + "'>";
   for (i = 0; i <= numGuests; i++) {
@@ -5,44 +54,6 @@ function generateSelectorHtml(label, numGuests, id) {
   }
   base += "</select>";
   return base;
-}
-
-function searchInvites() {
-  var firstname = $('#firstname').val();
-  var lastname = $('#lastname').val();
-  var searchUrl = "https://us-central1-round-carver-683.cloudfunctions.net" +
-    "/searchInvites?firstname=" + firstname + "&lastname=" + lastname;
-  $.get(searchUrl, function(data, status) {
-    var row = data;
-    if (row != '') {
-      $('#welcome').html(row[0] + ":\n" +
-        "\nWe're delighted to welcome you on our special day!" +
-        "\nHow many guests from your party should we expect?");
-      if (row[3] > 0) {
-        var friday_html = generateSelectorHtml('Rehearsal Dinner', row[3], 'friday');
-        var saturday_html = generateSelectorHtml('Wedding & Reception', row[4], 'saturday');
-        var sunday_html = generateSelectorHtml('Sunday Brunch', row[5], 'sunday');
-        $('#forms').html(friday_html);
-        $('#forms').append(saturday_html);
-        $('#forms').append(sunday_html);
-      } else if (row[3] == 0 && row[5] > 0) {
-        var saturday_html = generateSelectorHtml('Wedding & Reception', row[4], 'saturday');
-        var sunday_html = generateSelectorHtml('Sunday Brunch', row[5], 'sunday');
-        $('#forms').html(saturday_html);
-        $('#forms').append(sunday_html);
-      } else {
-        var saturday_html = generateSelectorHtml('Wedding & Reception', row[4], 'saturday');
-        $('#forms').html(saturday_html);
-      }
-      $('#forms').append("<br><br>Please inform us of any dietary restrictions: <input id='diet'></input>");
-      $('#forms').append("<button type='button' id='submitrsvp'>Submit RSVP!</button>");
-      $('#submitrsvp').click(function() {
-        submitRSVP(row[0]);
-      });
-    } else {
-      $('#error').css("display", "block");
-    }
-  });
 }
 
 function submitRSVP(guestName) {
@@ -60,3 +71,8 @@ function submitRSVP(guestName) {
     $('#welcome').html('RSVP Submitted!');
   });
 }
+
+$('#findinvites').click(function() {
+  $('#error').css("display", "hidden");
+  searchInvites();
+});
--- rsvp.md
+++ rsvp.md
@@ -1,18 +1,29 @@
 ---
 layout: rsvp
 ---
-If you've somehow reached this page please note that it is currently not working!
+If you've somehow reached this page, please note that it is currently not working!
 
 Do not RSVP yet, please. :)
 
-<input id="firstname" type="text" placeholder="First Name">
-<input id="lastname" type="text" placeholder="Last Name">
-<button type="button" onclick="searchInvites()">Find Invitation</button>
-<div id='welcome'></div>
+<div style="float:left;margin-right:20px;">
+    <label for="firstname">First Name</label>
+    <input id="firstname" type="text" placeholder="First Name">
+</div>
+<div style="float:left;margin-right:20px;">
+    <label for="lastname">Last Name</label>
+    <input id="lastname" type="text" placeholder="Last Name">
+</div>
+<div style="float:left;">
+    <button type="button" id="findinvite">Find Invitation</button>
+</div>
+<div id='welcome' style="display: none">
+We're delighted to welcome you on our special day!<br>
+How many guests from your party should we expect?
+</div>
 <div id='forms'></div>
 <div id='error' style="display: none">
-Sorry, we couldn't find your invitation.<br>
+Sorry, we couldn't find your invitation. 😢 <br>
 Is there an alternate spelling of your name?<br>
 If you continue to have trouble, just email me your RSVP directly at:<br>
-<em>trevor dot narayan at gmail dot com</em>. ❤️
+<em>trevor dot narayan at gmail dot com</em>.
 </div>
